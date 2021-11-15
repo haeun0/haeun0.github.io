e496c2c33ca399b1bc121d419f7788925dd6c920
haeun0 <89637861+haeun0@users.noreply.github.com>
2021-11-15 12:50:55 +0900

    홈페이지 입니다
    홈페이지 업로드

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.load = function () {};
+$(document).ready(function () {
+    // 위로가기 기능
+    $('.gotop').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        });
+    });
+
+});
